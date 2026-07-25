b278729e6844e42813e74d9020978b7985919cc9
szczyppka <p.szczypka.dev@gmail.com>
2026-07-25 17:22:57 +0200

    feat: add users module with GET/POST endpoints and validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), HealthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), HealthModule, UsersModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
